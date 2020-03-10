8c4f5777ad50c1c95de2e78ac1ea3b181c3d38a6
Justine Fricou <jfricou@justine.fricou>
2020-03-10 18:58:05 +0100

    MODIFY coordinates.c : Optimization of algorithm (path taking less detours)

--- solver/src/coordinates.c
+++ solver/src/coordinates.c
@@ -9,8 +9,11 @@
 
 int go_to_cell(maze_t maze, coord_t *pos, int wanted_nb)
 {
-    coord_t shifts[4] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    coord_t shifts_go[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    coord_t shifts_back[4] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    coord_t *shifts = NULL;
 
+    shifts = (wanted_nb == -2) ? shifts_go : shifts_back;
     for (int i = 0; i < 4; i++) {
         if (update_coords(maze, pos, shifts[i], wanted_nb))
             return (1);
@@ -20,11 +23,14 @@ int go_to_cell(maze_t maze, coord_t *pos, int wanted_nb)
 
 int update_coords(maze_t maze, coord_t *pos, coord_t shift, int wanted_nb)
 {
+    int cell = 0;
+
     if (pos->x + shift.x < 0 || pos->y + shift.y < 0)
         return (0);
     if (pos->x + shift.x >= maze.nb_lines || pos->y + shift.y >= maze.nb_cols)
         return (0);
-    if (maze.array[pos->x + shift.x][pos->y + shift.y] == wanted_nb) {
+    cell = maze.array[pos->x + shift.x][pos->y + shift.y];
+    if (cell == wanted_nb || (cell <= wanted_nb && cell >= 0)) {
         pos->x += shift.x;
         pos->y += shift.y;
         return (1);
