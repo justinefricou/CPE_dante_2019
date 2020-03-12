e97f7871475fd4ca733782eaf5e848db170c80f1
Justine Fricou <jfricou@justine.fricou>
2020-03-12 11:04:57 +0100

    MODIFY get_maze.c, main.c : replace printf by write for generator for generator

--- generator/main.c
+++ generator/main.c
@@ -11,15 +11,16 @@ int main(int argc, char **argv)
 {
     char *maze = NULL;
     coord_t dimensions = {0, 0};
+    int length = 0;
 
     srandom(time(NULL));
     if (detect_errors(argc, argv))
         return (84);
     if (get_dimensions(&(argv[1]), &dimensions) == 84)
         return (84);
-    if (get_maze(&maze, dimensions, (argc == 4)) == 84)
+    if (get_maze(&maze, dimensions, (argc == 4), &length) == 84)
         return (84);
-    printf("%s", maze);
+    write(1, maze, length);
     free(maze);
     return (0);
 }
